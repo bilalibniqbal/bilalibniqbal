fe9bb6c7a01f59068558750fe03954eaeb7ccfad
Muhammad Bilal <muhammadbilal.d14@gmail.com>
2024-04-30 23:30:42 +0500

    Add Hero section to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,32 @@
 import Image from "next/image";
+// import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero-section/Hero';
 
 export default function Home() {
-  return (
-    <div>
-        
-    </div>
-      );
+    return (
+        <div>
+            <div>
+                <div className="relative z-50">
+                    <div className="absolute">{/* <Navbar /> */}</div>
+                </div>
+            </div>
+
+            <div>
+                <div className="overflow-hidden">
+                    <div className="relative">
+                        <img
+                            className="absolute top-0 transition ease-in duration-200 max-xl:right-[-1050px] xl:right-[-970px] -z-30 image"
+                            width="4377"
+                            src="https://github.githubassets.com/images/modules/site/home-campaign/hero-bg.webp"
+                            // src="/images/stars1.png"
+                            alt=""
+                        />
+                    </div>
+                    <div className="hero-section px-3 ">
+                        <Hero />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
